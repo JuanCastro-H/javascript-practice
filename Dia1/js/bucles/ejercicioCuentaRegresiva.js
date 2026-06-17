ac6a1aa2f334e3f3eaf6efb77a212af4e47c2117
Juan Castro <juamcastroh@gmail.com>
2026-06-17 03:22:12 -0300

    Agregar ejercicio sobre bucles

---
+++ Dia1/js/bucles/ejercicioCuentaRegresiva.js
@@ -0,0 +1,17 @@
+/* 
+* Realizar en Javascript la cuenta regresiva para el año nuevo,
+comenzar en 10 y terminar en 0.
+
+* Al finalizar, debe mostrarse el mensaje "Feliz año nuevo".
+
+*/
+alert("hola")
+
+for(var contador = 10 ; contador >= 1 ; contador--){
+
+    console.log("El año nuevo comienz en: " + contador);
+
+    if (contador == 1){
+        alert("Feliz año nuevo");
+    }
+}
