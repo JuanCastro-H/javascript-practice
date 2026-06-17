e01f2ca66f5526626cc31b76bed455eafbf4c72e
Juan Castro <juamcastroh@gmail.com>
2026-06-17 02:34:09 -0300

    Agregar reto basico de condicionales donde se verifica si un valor estatico es verdadero o falso

---
+++ Dia1/js/aprobado.js
@@ -0,0 +1,10 @@
+var aprobe = false;
+
+
+if (aprobe == true) {
+    console.log("Puedo salir!!")
+} else {
+    console.log("Tengo que estudiar mirando videos de TodoCode")
+}
+
+var color = prompt("Escribi un color primario");
