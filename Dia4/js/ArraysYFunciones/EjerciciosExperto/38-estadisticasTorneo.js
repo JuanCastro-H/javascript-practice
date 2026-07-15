7baee35daf58dfeb7a70147251578396de65338f
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:39:22 -0300

    Guardar tercer ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/38-estadisticasTorneo.js
@@ -0,0 +1,32 @@
+/************************************************
+ARCHIVO: 38-estadisticas-torneo.js
+EJERCICIO 38 — ESTADÍSTICAS DE TORNEO
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Un torneo registra los puntos obtenidos por
+cada jugador.
+
+Debes calcular:
+
+- Ranking.
+- Promedio.
+- Top 3.
+- Puntaje total.
+
+Métodos recomendados:
+
+- sort
+- reduce
+- slice
+
+Requisitos mínimos:
+
+- Crear una función por cada cálculo.
+
+Bonus:
+
+- Mostrar también el jugador con menor puntaje.
+*/
