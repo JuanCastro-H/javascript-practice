aa86b1a3d12d197e3501286b9c3dc6a6a885fcd5
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:39:45 -0300

    Guardar cuarto ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/39-gestorTareas.js
@@ -0,0 +1,29 @@
+/************************************************
+ARCHIVO: 39-gestor-tareas.js
+EJERCICIO 39 — GESTOR DE TAREAS
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una aplicación almacena tareas.
+
+Cada tarea posee:
+
+- nombre
+- completada
+
+Debes:
+
+- Verificar si existen tareas pendientes.
+- Obtener únicamente las pendientes.
+
+Métodos recomendados:
+
+- some
+- filter
+
+Bonus:
+
+- Mostrar cuántas tareas faltan.
+*/
