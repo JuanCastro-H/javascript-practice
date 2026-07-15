be9821ffbc7a1f8c0bbdfcdd667aa39aa367ae9c
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:38:17 -0300

    Guardar segundo ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/37-tiendaOnline.js
@@ -0,0 +1,29 @@
+/************************************************
+ARCHIVO: 37-tienda-online.js
+EJERCICIO 37 — TIENDA ONLINE
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una tienda online posee un inventario de productos.
+
+Debes obtener:
+
+- Productos disponibles.
+- Productos agotados.
+- Inventario ordenado por precio.
+
+Métodos recomendados:
+
+- filter
+- sort
+
+Requisitos mínimos:
+
+- Utilizar funciones flecha.
+
+Bonus:
+
+- Mostrar únicamente los cinco productos más baratos.
+*/
