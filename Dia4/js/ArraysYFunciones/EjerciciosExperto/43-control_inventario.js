42487855332a55411117d1b9a4f24bb2f505a908
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:41:00 -0300

    Guardar octavo ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/43-control_inventario.js
@@ -0,0 +1,27 @@
+/************************************************
+ARCHIVO: 43-control-inventario.js
+EJERCICIO 43 — CONTROL DE INVENTARIO
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una tienda recibe nuevos productos,
+vende algunos y elimina aquellos
+que quedaron sin stock.
+
+Métodos recomendados:
+
+- push
+- splice
+- filter
+
+Requisitos mínimos:
+
+- Crear funciones independientes para
+cada operación.
+
+Bonus:
+
+- Registrar cada movimiento realizado.
+*/
