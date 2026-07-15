8954a981609be9f6e4bfb74e9a489b4a3b3cb36f
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:40:05 -0300

    Guardar quinto ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/40-controlAsistencia.js
@@ -0,0 +1,30 @@
+/************************************************
+ARCHIVO: 40-control-asistencia.js
+EJERCICIO 40 — CONTROL DE ASISTENCIA
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Un profesor registra la asistencia de sus alumnos.
+
+Cada alumno posee:
+
+- nombre
+- presente
+
+Debes calcular:
+
+- Cantidad de presentes.
+- Cantidad de ausentes.
+- Porcentaje de asistencia.
+
+Métodos recomendados:
+
+- filter
+- reduce
+
+Bonus:
+
+- Mostrar los nombres de los ausentes.
+*/
