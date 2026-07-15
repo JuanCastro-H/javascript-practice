1863f71f4eb359da4382513727d9c7f83474f205
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:42:24 -0300

    Guardar decimo segundo ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/47-sistemaEnvios.js
@@ -0,0 +1,27 @@
+/************************************************
+ARCHIVO: 47-sistema-envios.js
+EJERCICIO 47 — SISTEMA DE ENVÍOS
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una empresa de logística administra
+una cola de paquetes.
+
+Debes:
+
+- Enviar el primer paquete.
+- Registrar nuevos paquetes.
+- Verificar si quedan envíos urgentes.
+
+Métodos recomendados:
+
+- shift
+- push
+- some
+
+Bonus:
+
+- Procesar varios envíos seguidos.
+*/
