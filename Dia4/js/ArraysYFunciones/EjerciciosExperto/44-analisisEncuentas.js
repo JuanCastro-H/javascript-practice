017773d547ee23bd6df2d0b555eeb4dab44c12fe
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:41:21 -0300

    Guardar noveno ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/44-analisisEncuentas.js
@@ -0,0 +1,29 @@
+/************************************************
+ARCHIVO: 44-analisis-encuestas.js
+EJERCICIO 44 — ANÁLISIS DE ENCUESTAS
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una empresa realizó una encuesta
+de satisfacción.
+
+Debes calcular:
+
+- Promedio.
+- Cantidad de respuestas positivas.
+- Cantidad de respuestas negativas.
+- Porcentaje de respuestas positivas.
+
+Métodos recomendados:
+
+- reduce
+- filter
+- some
+
+Bonus:
+
+- Mostrar una conclusión automática
+según los resultados.
+*/
