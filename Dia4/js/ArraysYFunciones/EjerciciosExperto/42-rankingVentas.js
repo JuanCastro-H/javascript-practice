e16c957e2605e2a5f6d0aa667510a4ae56f53b4d
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:40:42 -0300

    Guardar septimo ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/42-rankingVentas.js
@@ -0,0 +1,23 @@
+/************************************************
+ARCHIVO: 42-ranking-ventas.js
+EJERCICIO 42 — RANKING DE VENTAS
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una empresa desea conocer sus productos
+más vendidos.
+
+Debes mostrar un Top 10.
+
+Métodos recomendados:
+
+- sort
+- slice
+- map
+
+Bonus:
+
+- Mostrar también la posición del ranking.
+*/
