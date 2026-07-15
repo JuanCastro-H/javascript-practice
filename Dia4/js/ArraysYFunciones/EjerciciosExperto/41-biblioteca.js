1e5c9829f28883907773d639ce21496cb246eb8a
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:40:22 -0300

    Guardar sexto ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/41-biblioteca.js
@@ -0,0 +1,26 @@
+/************************************************
+ARCHIVO: 41-biblioteca.js
+EJERCICIO 41 — SISTEMA DE BIBLIOTECA
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una biblioteca almacena cientos de libros.
+
+Debes generar un catálogo únicamente con
+los libros disponibles.
+
+El catálogo debe quedar ordenado
+alfabéticamente.
+
+Métodos recomendados:
+
+- filter
+- sort
+- map
+
+Bonus:
+
+- Mostrar solamente los títulos.
+*/
