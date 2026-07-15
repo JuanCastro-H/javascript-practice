ebfe29d14083402e1a336bf0d3bdc014625046a1
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:41:46 -0300

    Guardar decimo ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/45-estadisticasVideojuegos.js
@@ -0,0 +1,27 @@
+/************************************************
+ARCHIVO: 45-estadisticas-videojuegos.js
+EJERCICIO 45 — ESTADÍSTICAS DE VIDEOJUEGOS
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Un videojuego registra la puntuación
+de todas las partidas.
+
+Debes obtener:
+
+- Puntaje máximo.
+- Puntaje mínimo.
+- Promedio.
+
+Métodos recomendados:
+
+- sort
+- reduce
+
+Bonus:
+
+- Mostrar cuántas partidas superaron
+el promedio.
+*/
