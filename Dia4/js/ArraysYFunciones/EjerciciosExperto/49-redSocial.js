53a8c98156f3e4ba9f763e39a4dca160c5a09e54
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:43:04 -0300

    Guardar decimo cuarto ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/49-redSocial.js
@@ -0,0 +1,35 @@
+/************************************************
+ARCHIVO: 49-red-social.js
+EJERCICIO 49 — ANALIZADOR DE RED SOCIAL
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una red social registra sus usuarios.
+
+Cada usuario posee:
+
+- nombre
+- publicaciones
+- activo
+
+Debes obtener:
+
+- Usuarios activos.
+- Promedio de publicaciones.
+- Ranking de usuarios más activos.
+- Top 5.
+
+Métodos recomendados:
+
+- filter
+- reduce
+- sort
+- slice
+
+Bonus:
+
+- Mostrar únicamente los nombres
+del Top 5.
+*/
