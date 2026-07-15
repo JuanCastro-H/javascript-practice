6eb688316d34ff6376a10bfe5a234fbb47c94786
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:37:50 -0300

    Guardar primer ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/36-sistemaVentas.js
@@ -0,0 +1,47 @@
+/************************************************
+ARCHIVO: 36-sistema-ventas.js
+EJERCICIO 36 — SISTEMA DE VENTAS
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una tienda registra todas las ventas realizadas
+durante el día.
+
+Cada venta posee:
+
+- nombre del producto
+- precio
+- cantidad vendida
+
+Debes generar un reporte con:
+
+- Productos vendidos.
+- Ingreso total.
+- Promedio de venta.
+- Productos cuyo total vendido sea mayor a 500.
+
+Entrada:
+
+[
+    { producto: "Teclado", precio: 150, cantidad: 2 },
+    { producto: "Mouse", precio: 80, cantidad: 5 },
+    { producto: "Monitor", precio: 900, cantidad: 1 }
+]
+
+Métodos recomendados:
+
+- map
+- reduce
+- filter
+
+Requisitos mínimos:
+
+- Crear al menos 3 funciones.
+- Cada función debe encargarse de una tarea específica.
+
+Bonus:
+
+- Ordenar el reporte por mayor ingreso generado.
+*/
