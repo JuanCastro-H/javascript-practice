b6baf08e1a2842565ad5c72f48c8b13a61aad314
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:42:05 -0300

    Guardar decimo primero ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/46-plataformaEducativa.js
@@ -0,0 +1,28 @@
+/************************************************
+ARCHIVO: 46-plataforma-educativa.js
+EJERCICIO 46 — PLATAFORMA EDUCATIVA
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una plataforma educativa almacena
+las notas de sus alumnos.
+
+Debes generar un informe con:
+
+- Aprobados.
+- Reprobados.
+- Promedio.
+- Verificar si todos aprobaron.
+
+Métodos recomendados:
+
+- filter
+- reduce
+- every
+
+Bonus:
+
+- Mostrar el alumno con mejor nota.
+*/
