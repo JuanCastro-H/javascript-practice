1de7881e590a65ef1f3b47d8c2930a41cf9cd4c5
Juan Castro <juamcastroh@gmail.com>
2026-07-14 23:42:48 -0300

    Guardar decimo tercero ejercicio experto

---
+++ Dia4/js/ArraysYFunciones/EjerciciosExperto/48-historialBancario.js
@@ -0,0 +1,32 @@
+/************************************************
+ARCHIVO: 48-historial-bancario.js
+EJERCICIO 48 — HISTORIAL BANCARIO
+Dificultad: Experto
+************************************************
+
+Problema:
+
+Una cuenta bancaria registra todos
+sus movimientos.
+
+Cada movimiento posee:
+
+- tipo
+- monto
+
+Debes calcular:
+
+- Saldo final.
+- Total de depósitos.
+- Total de retiros.
+
+Métodos recomendados:
+
+- filter
+- map
+- reduce
+
+Bonus:
+
+- Mostrar el movimiento de mayor monto.
+*/
