690f56c8047598ed43b7af2435c431bfc4b9eaeb
Juan Castro <juamcastroh@gmail.com>
2026-07-15 23:54:27 -0300

    Completar ejercicio 24

--- Dia4/js/ArraysYFunciones/EjerciciosAvanzados/24-rankingVideojuegos.js
+++ Dia4/js/ArraysYFunciones/EjerciciosAvanzados/24-rankingVideojuegos.js
@@ -23,3 +23,20 @@ Bonus:
 
 - Permitir elegir cuántos jugadores mostrar.
 */
+
+var puntuaciones = [500, 1200, 850, 300, 2100, 950, 700];
+
+function topPuntajes (puntuaciones, cantidadJugadores){
+
+    puntuaciones.sort((a, b) => b - a);
+
+    let top = puntuaciones.slice(0, cantidadJugadores);
+
+    return top;
+};
+
+const tamanioTop = Number(window.prompt("Ingrese el tamanio del top"));
+
+const topPuntuaciones = topPuntajes(puntuaciones, tamanioTop);
+
+alert("El top de puntuaciones fue: " + topPuntuaciones);
