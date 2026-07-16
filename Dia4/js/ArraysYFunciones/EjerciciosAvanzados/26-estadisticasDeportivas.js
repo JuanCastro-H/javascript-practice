c1fbffe90b652478c249803cd608fe9c0231a8a1
Juan Castro <juamcastroh@gmail.com>
2026-07-15 23:57:16 -0300

    feat: Crear funcion para calcular los puntos totales

--- Dia4/js/ArraysYFunciones/EjerciciosAvanzados/26-estadisticasDeportivas.js
+++ Dia4/js/ArraysYFunciones/EjerciciosAvanzados/26-estadisticasDeportivas.js
@@ -26,3 +26,18 @@ Bonus:
 
 - Determinar si el promedio supera 85 puntos.
 */
+
+var puntosPartidos = [55, 90, 75, 100, 95];
+
+function puntosTotales (puntosPartidos){
+
+    let puntosTotales = puntosPartidos.reduce((acomulador, puntaje) => {
+        acomulador = acomulador + puntaje;
+        return acomulador;
+    }, 0);
+
+    return puntosTotales;
+};
+
+
+
