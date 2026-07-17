1614881aebb345952dbdb8ed14e1d0b151e6ae71
Juan Castro <juamcastroh@gmail.com>
2026-07-16 23:48:45 -0300

    Realizar ejercicio de practica registro de temperaturas

--- Dia4/js/ArraysYFunciones/EjerciciosAvanzados/28-registroTemperaturas.js
+++ Dia4/js/ArraysYFunciones/EjerciciosAvanzados/28-registroTemperaturas.js
@@ -24,3 +24,19 @@ Bonus:
 
 - Mostrar también las tres más bajas.
 */
+
+var temperaturas = [32, 25, 40, 18, 37, 29];
+
+function obtenerTemperaturaAlta (temperaturas){
+
+    temperaturas.sort((a,b) => b - a);
+
+    const topTemperaturas = temperaturas.slice(0, 2);
+
+    console.log("Top 3 Temperaturas mas altas");
+    temperaturas.forEach(element => {
+        console.log(element)
+    });
+};
+
+obtenerTemperaturaAlta(temperaturas);
