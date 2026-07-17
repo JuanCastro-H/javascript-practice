0907666ebef5a51969ac2aa70fe7890c9a7340fc
Juan Castro <juamcastroh@gmail.com>
2026-07-16 23:51:35 -0300

    Realizar ejercicio de practica clasificacion de estudiantes

--- Dia4/js/ArraysYFunciones/EjerciciosAvanzados/30-clasificacionEstudiantes.js
+++ Dia4/js/ArraysYFunciones/EjerciciosAvanzados/30-clasificacionEstudiantes.js
@@ -25,3 +25,24 @@ Bonus:
 
 - Ordenar alfabéticamente.
 */
+
+var alumnos = [
+    { nombre: "Ana", nota: 8 },
+    { nombre: "Juan", nota: 4 },
+    { nombre: "Pedro", nota: 7 }
+];
+
+function AlumnosAprobados (alumnos){
+
+    let aprobados = alumnos.filter(alumno => alumno.nota >= 6);
+
+    let aprobadosMayusculas = aprobados.map(alumno => alumno.nombre.toUpperCase());
+
+    console.log(" --- Lista De Aprovados --- ");
+    aprobadosMayusculas.forEach(aprobado => {
+        console.log(aprobado);
+    });
+
+}
+
+alumnosAprobados();
