f31a728e3d95f13d177cf08abdba99096a5ec91c
Juan Castro <juamcastroh@gmail.com>
2026-07-19 23:13:27 -0300

    feat: Crear funcion para retirar canciones antiguas de la playlist

--- Dia4/js/ArraysYFunciones/EjerciciosAvanzados/34-listaReproduccion.js
+++ Dia4/js/ArraysYFunciones/EjerciciosAvanzados/34-listaReproduccion.js
@@ -22,3 +22,29 @@ Bonus:
 
 - Crear una copia antes de modificarla.
 */
+
+const playlist = [
+    { titulo: "Bohemian Rhapsody", anio: 1975 },
+    { titulo: "Imagine", anio: 1971 },
+    { titulo: "Hotel California", anio: 1976 },
+    { titulo: "Billie Jean", anio: 1983 },
+    { titulo: "Sweet Child O' Mine", anio: 1987 },
+    { titulo: "Smells Like Teen Spirit", anio: 1991 },
+    { titulo: "Wonderwall", anio: 1995 },
+    { titulo: "Viva la Vida", anio: 2008 },
+    { titulo: "Blinding Lights", anio: 2019 },
+    { titulo: "Flowers", anio: 2023 }
+];
+
+function retirarCancionesAntiguas (playlist){
+
+    const copiaPlaylist = playlist.slice();
+
+    let modernas = playlist.filter(cancion => cancion.anio > 2000);
+
+    return {
+        original : copiaPlaylist,
+        modernas : modernas
+    }
+}
+
