e85395df67a179b38332806343a6fa536f5554cc
Juan Castro <juamcastroh@gmail.com>
2026-07-19 23:04:29 -0300

    Completar ejercicio 33 revision de longitud de contrasenas

--- Dia4/js/ArraysYFunciones/EjerciciosAvanzados/33-revisionContrasenas.js
+++ Dia4/js/ArraysYFunciones/EjerciciosAvanzados/33-revisionContrasenas.js
@@ -25,3 +25,33 @@ Bonus:
 
 - Mostrar cuáles son inseguras.
 */
+
+var claves = ["abc123", "segura2026", "123", "1235", "89785"];
+
+function verificarLongitud (claves){
+
+    let inseguraIndice = [];
+
+    let longitud = claves.map(clave => {
+        return clave.length;
+    });
+
+    for (let i = 0; i<claves.length ; i++){
+        if (longitud[i] < 6){
+            inseguraIndice.push(claves[i]);
+        }
+    }
+
+    if (inseguraIndice.length > 0){
+        return inseguraIndice;
+    } else {
+        console.log("Todas las claves son seguras");
+        return [];
+    }
+};
+
+const clavesVerificadas = verificarLongitud(claves);
+if (clavesVerificadas.length > 0){
+    console.log(" --- Claves Inseguras --- ")
+    clavesVerificadas.forEach(clave => console.log(clave));
+}
